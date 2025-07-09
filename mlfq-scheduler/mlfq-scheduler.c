ff47933fa60539f3da7507d64e02f32003deff04
sockeye2204 <sockeye.2204@gmail.com>
2025-07-09 21:23:28 +0000

    Csv input

--- mlfq-scheduler/mlfq-scheduler.c
+++ mlfq-scheduler/mlfq-scheduler.c
@@ -16,20 +16,117 @@ within queues.
 
 struct Queue {
   int allotment;
+  struct Job* head;
+  int numJobs;
 };
 
 struct Job {
   int timeInQueue;
-  struct Queue* curQueue;
-  int runtime;
+  struct Job* next;
+  // Simulation-related - these are set values that would not be present in a
+  // 'real-time' system. Jobs would run for an unknown time, would enter the
+  // system at an unknown time - but for the sake of simulation we pre-
+  // define their run and entry times.
+  int runTime;
+  int entryTime;
 };
 
-static int gCounter = 0; // This is a 'pseudo' time-slice counter variable.
+static struct Queue sQueues[QUEUE_COUNT] = {0}; 
+
+static int sCounter = 0; // This is a 'pseudo' time-slice counter variable.
                          // We do not check real run-times, but run-times as
                          // given in the input that run for n increments of
                          // gCounter.
 
+static int sMainState = 0;
+
+static FILE* fptr = NULL;
+
+void ResetJobs(void)
+{
+}
+
+void AddJobToQueue(struct Job*, int queueNum)
+{
+}
+
+void CompleteJob(struct Job* job)
+{
+  
+}
+
 int main(int argc, char *argv[])
 {
-  return 0;
+  while(1)
+    {
+  switch(sMainState)
+    {
+    case 0:
+      if (argc != 2)
+	{
+	  printf("Usage: mlfq <csv-with-job-info>\n");
+	  return(1);
+	}
+      sMainState++;
+      break;
+    case 1:
+      fptr = fopen(argv[1], "r");
+
+      if (fptr == NULL)
+	{
+	  printf("Error: file could not be opened\n");
+	  return(2);
+	}
+      sMainState++;
+      break;
+    case 2:
+      char readBuffer[20];
+      struct Job* lastJob = NULL;
+
+      char* tokPtr;
+
+      while (fgets(readBuffer, 20, fptr)) {
+	struct Job* curJob = malloc(sizeof(struct Job));
+	if (curJob == NULL)
+	  {
+	    printf("Error: malloc failed for job.");
+	    return(5);
+	  }
+      tokPtr = strtok(readBuffer, ",");
+      if (tokPtr == NULL)
+	{
+	  printf("Line format: <runTime>,<entryTime>\n");
+	  return(3);
+	}
+      curJob->runTime = atoi(tokPtr);
+      tokPtr = strtok(NULL, ",");
+      if (tokPtr == NULL)
+	{
+	  printf("Line format: <runTime>,<entryTime>\n");
+	  return(4);
+	}
+      curJob->entryTime = atoi(tokPtr);
+      curJob->timeInQueue = 0;
+      if (lastJob != NULL)
+	{
+	  lastJob->next = curJob;
+	}
+      else
+	{
+	  sQueues[0].head = curJob;
+	}
+
+      sQueues[0].numJobs++;
+
+      printf("Job loaded: runTime %d, entryTime %d\n", curJob->runTime, curJob->entryTime);
+	    
+      lastJob = curJob;
+      }
+
+      sMainState++;
+      break;
+    case 3:
+      return(0); // Completed without error.
+    }
+    }
 }
