7be89b28f7317c8f9b88a14d047d3afb3808713b
sockeye2204 <sockeye.2204@gmail.com>
2025-07-14 22:25:30 +0000

    able to supply argument to time-tlb

--- time-tlb/time-tlb.c
+++ time-tlb/time-tlb.c
@@ -3,21 +3,27 @@
 #include <stdio.h>
 #include <time.h>
 
-#define MAX_PAGES 1024
-
 int main(int argc, char *argv[])
-{ 
+{
+  int max_pages = 1024;
+  
   int jump = getpagesize() / sizeof(int);
-  int *a = calloc(MAX_PAGES * jump, sizeof(int));
 
-  for (int i = 0; i < MAX_PAGES * jump; i++)
+  if (argc > 1)
+    {
+      max_pages = atoi(argv[1]);
+    }
+
+  int *a = calloc(max_pages * jump, sizeof(int));
+  
+  for (int i = 0; i < max_pages * jump; i++)
     {
       a[i] = 0;
     }
   
   struct timespec start, end;
   
-  for (int numpages = 1; numpages <= MAX_PAGES; numpages++)
+  for (int numpages = 1; numpages <= max_pages; numpages++)
     {
       clock_gettime(CLOCK_MONOTONIC, &start);
   for (int i = 0; i < numpages * jump; i += jump)
