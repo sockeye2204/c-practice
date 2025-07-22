0b0479813d028f2655b9837ec2c761bfcbefdf41
sockeye2204 <sockeye.2204@gmail.com>
2025-07-22 20:42:21 +0000

    Approximate lock

--- concurrency/lock-ds.c
+++ concurrency/lock-ds.c
@@ -4,12 +4,24 @@
 #include <sys/time.h>
 #include <string.h>
 
-#define THREAD_COUNT 8
+#define THREAD_COUNT 16
+#define MAXCPUS 4
 
+// Simple counter (one lock)
 typedef struct _scounter {
   int             val;
   pthread_mutex_t lock;
 } scounter;
+
+// Approximate counter (local locks & global lock)
+
+typedef struct _acounter {
+  int gval;
+  pthread_mutex_t glock;
+  int lvals[MAXCPUS];
+  pthread_mutex_t llocks[MAXCPUS];
+  int threshold;
+} acounter;
 
 // Wrappers (for catching mutex, thread function failures)
 
@@ -83,20 +95,67 @@ int get_scounter(scounter* sc)
   return ret;
 }
 
+// Approximate counter functions
+
+void init_acounter(acounter* ac, int threshold)
+{
+  ac->gval = 0;
+  ac->threshold = threshold;
+  Pthread_mutex_init(&ac->glock, NULL);
+  for (int i = 0; i < MAXCPUS; i++)
+    {
+      ac->lvals[i] = 0;
+      Pthread_mutex_init(&ac->llocks[i], NULL);
+    }
+}
+
+void inc_acounter(acounter* ac, int threadId, int amount)
+{
+  int cpu = threadId % MAXCPUS;
+  Pthread_mutex_lock(&ac->llocks[cpu]);
+  ac->lvals[cpu] += amount;
+  if (ac->lvals[cpu] >= ac->threshold)
+    {
+      Pthread_mutex_lock(&ac->glock);
+      ac->gval += ac->lvals[cpu];
+      Pthread_mutex_unlock(&ac->glock);
+      ac->lvals[cpu] = 0;
+    }
+  Pthread_mutex_unlock(&ac->llocks[cpu]);
+}
+
+int get_acounter(acounter *ac)
+{
+  Pthread_mutex_lock(&ac->glock);
+  int ret = ac->gval;
+  Pthread_mutex_unlock(&ac->glock);
+  return ret;
+}
+
 //
 
 void print_time(struct timeval* tv)
 {
-  printf("tv = %ld.%ld secs\n", tv->tv_sec, tv->tv_usec);
+  printf("tv = %ld.%06ld secs\n", tv->tv_sec, tv->tv_usec);
 }
 
 void* many_inc_scounter(void* arg)
 {
   scounter* sc = (scounter*)arg;
-  for (int i = 0; i < 10000000; i++)
+  while(get_scounter(sc) < 10000000)
     {
       inc_scounter(sc);
     }
+}
+
+void* many_inc_acounter(void* arg)
+{
+  acounter* ac = (acounter*)arg;
+  while(get_acounter(ac) < 10000000)
+    {
+      inc_acounter(ac, 1, pthread_self());
+    }
+  
 }
 
 void timeval_sub(struct timeval *tv2, struct timeval *tv1)
@@ -149,6 +208,44 @@ int main(int argc, char* argv[])
 	  gettimeofday(&tv2, NULL);
 	  timeval_sub(&tv2, &tv1);
 	  print_time(&tv2);
+	}
+    }
+  if (argc >= 2 && !(strcmp(argv[1], "ac")))
+    {
+      acounter ac;
+      printf("Performance as threshold increases (threads: %d)\n", THREAD_COUNT);
+      for (int i = 1; i <= 8192; i*=2)
+	{
+	  init_acounter(&ac, i);
+	  gettimeofday(&tv1, NULL);
+	  for (int j = 0; j < THREAD_COUNT; j++)
+	    {
+	      Pthread_create(&threads[j], NULL, many_inc_acounter, &ac);
+	    }
+	  for (int j = 0; j < THREAD_COUNT; j++)
+	    {
+	      pthread_join(threads[j], NULL);
+	    }
+	  gettimeofday(&tv2, NULL);
+	  timeval_sub(&tv2, &tv1);
+	  print_time(&tv2);	  
+	}
+      printf("Performance as thread count increases (threshold: 8)\n");
+      for (int i = 1; i <= THREAD_COUNT; i++)
+	{
+	  init_acounter(&ac, 8);
+	  gettimeofday(&tv1, NULL);
+	  for (int j = 0; j < i; j++)
+	    {
+	      Pthread_create(&threads[j], NULL, many_inc_acounter, &ac);
+	    }
+	  for (int j = 0; j < i; j++)
+	    {
+	      pthread_join(threads[j], NULL);
+	    }
+	  gettimeofday(&tv2, NULL);
+	  timeval_sub(&tv2, &tv1);
+	  print_time(&tv2);	  
 	}
     }
   return 0;
