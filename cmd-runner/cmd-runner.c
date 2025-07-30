62b847acc977a50e5b2d6da158bb41b5dbdf124e
sockeye2204 <214020432+sockeye2204@users.noreply.github.com>
2025-07-30 22:07:14 +0000

    cmd-runner: added .txt input

--- cmd-runner/cmd-runner.c
+++ cmd-runner/cmd-runner.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <wordexp.h>
+#include <sys/wait.h>
 
 #define MAX_LEN 200
 
@@ -16,28 +17,69 @@ int main(int argc, char* argv[])
       exit(1);
     }
 
-  cur_arg = 1;
-
-  while (cur_arg < argc)
+  if (argc == 3 && strcmp(argv[1], "-f") == 0)
     {
-      printf("Running: %s\n", argv[cur_arg]);
+      FILE* fptr = fopen(argv[2], "r");
 
-      char* this_str = argv[cur_arg];
+      char* this_str = malloc(sizeof(char)); // Gets realloc'd later anyway
+      size_t this_size = 0;
+      ssize_t chars_read = getline(&this_str, &this_size, fptr);
+      while (chars_read != -1)
+	{
+	  if (chars_read > 0 && this_str[chars_read - 1] == '\n')
+	    {
+	      this_str[chars_read - 1] = '\0';
+	    }
 
-      wordexp_t p;
-      wordexp(this_str, &p, 0);
+	  printf("\nRunning: %s\n", this_str);
 
-      pid_t pid = fork();
+	  wordexp_t p;
+	  wordexp(this_str, &p, 0);
 
-      if (pid < 0) {
-	fprintf(stderr, "Error: failed to fork()\n");
-	exit(2);
-      } else if (pid == 0) {
-	execvp(p.we_wordv[0], p.we_wordv);
-	wordfree(&p);
-      } else {
-      }
+	  pid_t pid = fork();
+
+	  if (pid < 0) {
+	    fprintf(stderr, "Error: failed to fork()\n");
+	    exit(2);
+	  } else if (pid == 0) {
+	    execvp(p.we_wordv[0], p.we_wordv);
+	    wordfree(&p);
+	  } else {
+	    wait(NULL);
+	  }
+	  
+	  chars_read = getline(&this_str, &this_size, fptr);	  
+	}
       
-      cur_arg++;
     }
+  else
+    {
+      cur_arg = 1;
+
+      while (cur_arg < argc)
+	{
+	  printf("Running: %s\n", argv[cur_arg]);
+
+	  char* this_str = argv[cur_arg];
+
+	  wordexp_t p;
+	  wordexp(this_str, &p, 0);
+
+	  pid_t pid = fork();
+
+	  if (pid < 0) {
+	    fprintf(stderr, "Error: failed to fork()\n");
+	    exit(2);
+	  } else if (pid == 0) {
+	    execvp(p.we_wordv[0], p.we_wordv);
+	    wordfree(&p);
+	  } else {
+	    wait(NULL);
+	  }
+
+	  cur_arg++;
+	}
+  }
+
+  return 0;
 }
